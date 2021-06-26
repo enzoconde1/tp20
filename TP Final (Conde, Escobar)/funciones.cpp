56e43f6133164493222971651ab9db43818167f5
enzoconde1 <enzofconde@outlook.com.ar>
2021-06-26 19:29:02 -0300

    menu y opciones actualizados

--- TP Final (Conde, Escobar)/funciones.cpp
+++ TP Final (Conde, Escobar)/funciones.cpp
@@ -16,52 +16,50 @@ void puntaje();
 void modoSimulacion();
 
 void Menu(){
-char n1;
-
-cout<< "Bienvenido al Juego de 20 :"<<endl;
-
-cout<< "-----------------------------------------------------------------------"<<endl;
-system("pause");
-cout<<endl<< "-----------------------------------------------------------------"<<endl;
-system("cls");
+    char input;
+    cout<< "Bienvenido al Juego de 20 :"<<endl;
+    cout<< "-----------------------------------------------------------------------"<<endl;
+    system("pause");
+    cout<<endl<< "-----------------------------------------------------------------"<<endl;
+    system("cls");
 }
 
 
 
 
 void Opciones(){
-char n1;
+    char input;
 
-cout<< "seleciones modo de juego :"<<endl;
+    cout << "Seleccione modo de juego :"<<endl;
 
-cout<< "-----------------------------------------------------------------------"<<endl;
-cout<< "ingrese 1 para modo de juego de una persona"<<endl;
-cout<< "ingrese 2 para modo de juego para dos personas"<<endl;
-cout<< "ingrese 3 para el modo simulacion"<<endl;
-system("pause");
+    cout << "-----------------------------------------------------------------------"<<endl;
+    cout << "Ingrese 1 para modo de juego de una persona"<<endl;
+    cout << "Ingrese 2 para modo de juego para dos personas"<<endl;
+    cout << "Ingrese 3 para mostrar puntuacion mas alta" << endl;
+    cout << "Ingrese 4 para el modo simulacion"<<endl;
+    system("pause");
 
 
-cout<<endl<< "------------------------------------------------------------------"<<endl;
-cout<<endl<< "ingrese uno de los 3 numeros para selecionar el modo de juego :";
-cin>>n1;
-cout<<endl<< "-----------------------------------------------------------------"<<endl;
+    cout<<endl<< "------------------------------------------------------------------"<<endl;
+    cout<<endl<< "Ingrese uno de los 4 numeros para ingresar :";
+    input = getch();
+    cout<<endl<< "-----------------------------------------------------------------"<<endl;
 
-while(n1<0 || n1>4 || isnan(n1)){
-            if(n1=='1' || n1=='2'|| n1=='3' || n1== '4'){
-                system("cls");
-            } else {
-                cout << "Ingrese un numero correcto del 1 al 4" << endl;
-                cin >> n1;
-            }
+    while(input<0 || input>4 || isnan(input)){
+        if(input=='1' || input=='2'|| input=='3' || input== '4'){
+            system("cls");
+        } else {
+            cout << "Ingrese un numero correcto del 1 al 4: ";
+            input = getch();
+        }
+        switch(input){
+            case '1': unJugador(); break;
+            case '2': dosJugadores(); break;
+            case '3': puntaje(); break;
+            case '4': modoSimulacion() ; break;
+        }
 
-            switch(n1){
-                case '1': unJugador(); break;
-                case '2': dosJugadores(); break;
-                case '3': puntaje(); break;
-                case '4': modoSimulacion() ; break;
-            }
-
-}
+    }
 }
 
 void unJugador() {
