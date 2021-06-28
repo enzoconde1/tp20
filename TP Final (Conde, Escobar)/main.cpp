240454b100aa0d963e75b4263a933195a0509974
enzoconde1 <enzofconde@outlook.com.ar>
2021-06-28 15:43:31 -0300

    prueba

--- TP Final (Conde, Escobar)/main.cpp
+++ TP Final (Conde, Escobar)/main.cpp
@@ -7,43 +7,14 @@
 #include<stdio.h>
 
 using namespace std;
-
-void Opciones(char n1){
-
-cout<< "seleciones modo de juego :"<<endl;
-
-cout<< "-----------------------------------------------------------------------"<<endl;
-cout<< "ingrese 1 para modo de juego de una persona"<<endl;
-cout<< "ingrese 2 para modo de juego para dos personas"<<endl;
-cout<< "ingrese 3 para el modo simulacion"<<endl;
-system("pause");
-cout<<endl<< "------------------------------------------------------------------"<<endl;
-cout<<endl<< "ingrese uno de los 3 numeros para selecionar el modo de juego :";
-cin>>n1;
-cout<<endl<< "-----------------------------------------------------------------"<<endl;
-
-switch(n1){
-    case 1:
-
-    break;
-    case 2:
-
-    break;
-    case 3:
-
-    break;
-
-    default:
-        cout<< "el numero no es correcto"<<endl;
-        break;
-}
-
-}
+using namespace rlutil;
 
 void Opciones();
+void Menu();
 
-int main()
-{char n1;
-Opciones(n1);
+int main() {
+
+Menu();
+Opciones();
     return 0;
 }
